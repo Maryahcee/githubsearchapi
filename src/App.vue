4d856ca58d39ce39c0170c67313206d62db0c8d8
MaryahKim <maryapitaz@gmail.com>
2022-05-18 17:55:27 +0300

    Update App.vue
    Delete router

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,21 @@ import { RouterLink, RouterView } from "vue-router";
 import Nav from "./components/Nav.vue";
 import SearchField from "./components/SearchField.vue";
 import Profile from "./components/Profile.vue";
-import { computed } from "@vue/reactivity";
-import { mapGetters, mapMutations } from "vuex";
 import { useStore } from "vuex";
+import { computed, onMounted } from "@vue/runtime-core";
 
 const store = useStore();
 
 const theme = computed(() => {
-  theme.mapGetters(["dark"]);
+  return store.getters.getTheme;
 });
 </script>
 
-
-
 <template>
   <div
-    :class="'dark' ? 'light' : 'dark'"
-    class="bg-lGrayish dark:bg-black dark:text-White h-screen max-w-full flex flex-col mx-auto items-center justify-center font-['Space_Mono']"
+    class="light:bg-white dark:bg-black min-h-screen max-w-full flex flex-col mx-auto items-center justify-center font-['Space_Mono']"
+    :class="theme"
+
   >
     <div>
       <nav>
@@ -29,6 +27,5 @@ const theme = computed(() => {
     <Nav />
     <search-field />
     <profile />
-    <RouterView />
   </div>
 </template>
