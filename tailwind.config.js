59573713b109b0e8af9a754734d6768c4bb6a320
MaryahKim <maryapitaz@gmail.com>
2022-05-18 14:41:17 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+  darkMode: 'class',
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    screens: {
+      'xm': '340px',
+      'sm': '640px',  
+      'md': '768px',  
+      'lg': '1024px',  
+      'xl': '1280px',  
+      '2xl': '1536px'
+    },
+    extend: {
+      colors:{
+        'White': '#FFFFFF',
+        'lGrayish': '#F6F8FF',
+        'lBlack': '#2B3442',
+        'lBlue': '#4B6A9B',
+        'lGrey': '#697C9A',
+        'lSky': '#0079FF',
+        'black': '#141D2F',
+        'darkBlue': '#1E2A47',
+        'bright': '#FEFEFE',
+      },
+      fontFamily: {
+        'space':['"Space Mono"', 'monospace']
+      },
+      
+    },
+  },
+  plugins: [],
+};
