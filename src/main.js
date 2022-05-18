1f9be191f862dcfada0c355d17eb13a0a5f51afe
MaryahKim <maryapitaz@gmail.com>
2022-05-18 14:39:58 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "./assets/tailwind.css";
+//import store
+import store from "./store/store";
 
 const app = createApp(App);
 
 app.use(router);
+
+app.use(store);
 
 app.mount("#app");
