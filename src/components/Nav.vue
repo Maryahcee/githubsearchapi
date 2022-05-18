563932d03471279159fa5ea5d87f550411ae22e2
MaryahKim <maryapitaz@gmail.com>
2022-05-18 17:55:40 +0300

    Update Nav.vue

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -17,21 +17,20 @@ const toggleTheme = () => {
 
 <template>
   <div
-    class="container w-10/12 md:w-3/4 lg:w-3/4 xm:mt-10 sm:mt-2 md:mt-3 cursor-pointer"
+    class="container w-10/12 md:w-3/4 lg:w-3/4 xm:mt-10 sm:mt-2 md:mt-3 cursor-pointer text-lBlack dark:text-bright"
   >
     <nav class="relative mb-3 font-['Space_Mono']">
-      <div class="">
+      <div class="text-lBlack dark:text-bright">
         <h1 class="text-2xl font-bold text-lBlack dark:text-bright">
           devfinder
         </h1>
       </div>
       <button
         @click="toggleTheme"
-       :class="'dark' ? 'light' : 'dark'"
         class="absolute right-0 top-0 flex cursor-pointer hover:text-lGrey"
       >
-        <span v-if="theme == 'dark'" class="flex">
-          <span class="text-md text-lBlack mr-3">DARK</span>
+        <span v-if="theme == 'light'" class="flex">
+          <span class="text-md text-lBlack dark:text-bright mr-3">DARK</span>
           <img src="../assets/images/icon-moon.svg" alt="" />
         </span>
 
