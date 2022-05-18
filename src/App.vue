e2183dc045b416fe945df0e73a48f6965c8f1102
MaryahKim <maryapitaz@gmail.com>
2022-05-18 14:37:01 +0300

    Update App.vue
    Import store

--- src/App.vue
+++ src/App.vue
@@ -1,125 +1,34 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
+import Nav from "./components/Nav.vue";
+import SearchField from "./components/SearchField.vue";
+import Profile from "./components/Profile.vue";
+import { computed } from "@vue/reactivity";
+import { mapGetters, mapMutations } from "vuex";
+import { useStore } from "vuex";
+
+const store = useStore();
+
+const theme = computed(() => {
+  theme.mapGetters(["dark"]);
+});
 </script>
 
+
+
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+  <div
+    :class="'dark' ? 'light' : 'dark'"
+    class="bg-lGrayish dark:bg-black dark:text-White h-screen max-w-full flex flex-col mx-auto items-center justify-center font-['Space_Mono']"
+  >
+    <div>
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/"></RouterLink>
       </nav>
     </div>
-  </header>
-
-  <RouterView />
+    <Nav />
+    <search-field />
+    <profile />
+    <RouterView />
+  </div>
 </template>
-
-<style>
-@import "@/assets/base.css";
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
